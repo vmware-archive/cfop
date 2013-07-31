e892bed6bd53454aaface25776d51206f2ce055c
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-07-30 22:56:39 -0700

    Remove unneeded files

--- share/lib/cfop/common.rb
+++
@@ -1,9 +0,0 @@
-module CfOp
-  module Common
-    class << self
-      def find_prod_yaml_file
-        ENV.fetch("CFOP_AWS_STUB", File.expand_path("~/workspace/prod-aws/cf-aws-stub.yml"))
-      end
-    end
-  end
-end
